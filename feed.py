3f3d8861563155e5397a14286756e4f68241ab5f
pufferenco <sanderalberts11@gmail.com>
2025-03-13 23:04:03 +0100

    changes

--- feed.py
+++ feed.py
@@ -2,4 +2,4 @@ from globals import *
 
 @app.route('/getPost/<str:authToken>', methods=['GET'])
 def feed(authToken: str) -> str:
-    return getFullPage(render_template("index.html", posts=Posts))AW
+    return getFullPage(render_template("index.html", posts=Posts))
