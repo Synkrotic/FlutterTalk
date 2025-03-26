032cd5fb5eab43f99432621c6c99f74ddc116567
pufferenco <sanderalberts11@gmail.com>
2025-03-26 13:54:47 +0100

    shit

--- feed.py
+++
@@ -1,7 +0,0 @@
-from globals import *
-
-
-
-@app.route('/getPost/<str:authToken>', methods=['GET'])
-def feed(authToken: str) -> str:
-    return getFullPage(render_template("index.html", posts=Posts))
