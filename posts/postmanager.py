b179343d2a8e67abc8623249692aaedf6dec3dbb
pufferenco <sanderalberts11@gmail.com>
2025-04-17 00:18:55 +0200

    revert

--- posts/postmanager.py
+++ posts/postmanager.py
@@ -83,10 +83,9 @@ def getSubset(request: Request, session: Session) -> Query | None:
 
 def getPosts(amount: int, request: Request) -> (dict, list[Cookie]):
     currentPost = 0
-    if request.args.get('restart') is not None or getCookie(request, 'current_post') is not None:
-        cookies = addCookie([], Cookie("current_post", currentPost + amount))
-    else:
+    if getCookie(request, 'current_post') is not None:
         currentPost = getCookie(request, 'current_post')
+    cookies = addCookie([], Cookie("current_post", currentPost + amount))
     
     with database.getSession() as session:
         posts = getSubset(request, session)\
